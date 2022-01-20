823ad9b0b9c378e64e0bb18dd4ae393bc152bc18
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2022-01-19 14:55:09 -1000

    adjusted menu items based on sign-in; added message modal

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Image, Input, Icon } from 'semantic-ui-react';
-import { Roles } from 'meteor/alanning:roles';
-import { ROLE } from '../../api/role/Role';
+import { Menu, Dropdown, Image, Input, Icon, Modal, Button, Grid, Header } from 'semantic-ui-react';
+// import { Roles } from 'meteor/alanning:roles';
+// import { ROLE } from '../../api/role/Role';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 const NavBar = ({ currentUser }) => {
   const menuStyle = { marginBottom: '25px', paddingBottom: '2rem', paddingTop: '2rem', backgroundColor: '#FFFFFF' };
   const input = { width: '30rem' };
+  const [open, setOpen] = React.useState(false);
   return (
     <Menu secondary style={menuStyle} attached="top" borderless>
       <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/">
@@ -22,17 +23,49 @@ const NavBar = ({ currentUser }) => {
       <Menu.Item as={NavLink} activeClassName="" exact to="/notfound" key='browse'>Browse Opportunities</Menu.Item>
       <Menu.Item as={NavLink} activeClassName="" exact to="/notfound" key='library'>Organization Library</Menu.Item>
       <Menu.Item as={NavLink} activeClassName="" exact to="/notfound" key='about'>About Us</Menu.Item>
-      <Menu.Item as={NavLink} activeClassName="" exact to="/notfound" key='signin'>Sign Up</Menu.Item>
       {currentUser === '' ? (
-        <Menu.Item as={NavLink} exact to="/signin"><Icon name='user'/>Sign In</Menu.Item>
+        [<Menu.Item as={NavLink} activeClassName="" exact to="/notfound" key='signup'>Sign Up</Menu.Item>,
+          <Menu.Item as={NavLink} exact to="/signin" key='signin'><Icon name='user'/>Sign In</Menu.Item>]
       ) : (
         [<Menu.Item key="user">
-          <Icon name='user'/>
-          <Dropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} text="Login" pointing="top right">
+          <Image src='https://volunteerally.org/wp-content/uploads/2021/08/VA-logo-circle-v5.svg' avatar/>
+          <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right">
             <Dropdown.Menu>
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_UP} icon="sign out" text="Sign Out" as={NavLink} exact to="/signout" />
+              <Dropdown.Item activeClassName="" text="Add Opportunity" as={NavLink} exact to="/notfound"/>
+              <Dropdown.Item activeClassName="" text="My Opportunity" as={NavLink} exact to="/notfound"/>
+              <Dropdown.Item activeClassName="" text="My Profile" as={NavLink} exact to="/notfound"/>
+              <Dropdown.Item activeClassName="" text="My Organization Profile" as={NavLink} exact to="/notfound"/>
+              <Dropdown.Item activeClassName="" text="Account Settings" as={NavLink} exact to="/notfound"/>
+              <Dropdown.Item activeClassName="" id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Logout" as={NavLink} exact to="/signout" />
             </Dropdown.Menu>
           </Dropdown>
+        </Menu.Item>,
+        <Menu.Item key="messages" activeClassName="">
+          <Modal
+            closeIcon
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={<Button basic circular><Icon name="comments" color="black"/></Button>}
+            key='messages'>
+            <Modal.Header>Messages</Modal.Header>
+            <Grid container doubling centered verticalAlign='middle'>
+              <div className="modal-margin">
+                <Grid.Column textAlign='center'>
+                  <Icon name="inbox" size="big"/>
+                  <Header as='h3'>No Messages Available</Header>
+                </Grid.Column>
+              </div>
+            </Grid>
+            <Modal.Actions>
+              <Button
+                content="Compose"
+                labelPosition='right'
+                color="blue"
+                icon="plus"
+                onClick={() => setOpen(false)}/>
+            </Modal.Actions>
+          </Modal>
         </Menu.Item>]
       )}
     </Menu>
