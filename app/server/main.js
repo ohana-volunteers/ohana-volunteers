2cb7cc23d5dfc397c1a9fd6dbc01a17275c027f9
yongxin <yongxin@hawaii.edu>
2022-02-01 02:14:51 -1000

    sign-up page
    Modified the format;
    Dealt with checkbox and radio;

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,16 @@
+import { Meteor } from 'meteor/meteor';
 import '/imports/startup/server/Accounts';
 import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
 // be sure to import the methods.
 import '../imports/api/base/BaseCollection.methods';
+import { Roles } from 'meteor/alanning:roles';
+import { ROLE } from '../imports/api/role/Role';
+
+Meteor.methods({
+  assignRole() {
+    const role = ROLE.USER;
+    Roles.createRole(role, { unlessExists: true });
+    Roles.addUsersToRoles(Meteor.user(), [role]);
+  },
+});
