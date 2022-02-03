d757b61cdf2569cdd9cb01caeaf63da2cb536f69
Andrew Emmons <60409142+andrewprograms@users.noreply.github.com>
2022-02-03 11:39:27 -0800

    Update Organizations.jsx

--- app/imports/ui/pages/Organizations.jsx
+++ app/imports/ui/pages/Organizations.jsx
@@ -3,6 +3,12 @@ import {Grid, Header, Card, Divider, Item, Icon, Button, Container, Image, Dropd
 
 const container1Style = { backgroundColor: 'blue', paddingBottom: '325px', marginTop: '-25px', paddingLeft: '0px'};
 const textStyle = { color: 'white', marginTop: '150px'};
+const textStyle2 = { color: 'blue', textAlign: 'center', paddingTop: '50px'};
+const textStyle3 = { textAlign: 'center', paddingTop: '50px'};
+const iconStyle = { paddingLeft: '115px'};
+const iconTextStyle = { paddingLeft: '80px', fontsize: '30px'};
+const gridStyle = { paddingTop: '30px'};
+const gridStyle2 = { paddingTop: '30px', paddingBottom: '120px'};
 /** A simple static component to render some text for the About Us page. */
 const Organizations = () => (
     <div>
@@ -70,6 +76,53 @@ const Organizations = () => (
                     <Image src='org-logos-16.png' />
                 </Grid.Column>
             </Grid.Row>
+        </Grid>
+
+        <Image src='elissa-garcia-MV1l4f_f1os-unsplash-2048x1365.jpeg' fluid />
+
+        <Item.Header as="h1" style={textStyle2}> Join over 20 organizations already finding the help they need with Volunteer Ally. </Item.Header>
+
+        <Item.Description style={textStyle3}> There are thousands of active volunteers waiting for opportunities to work with qualified organizations. Make sure your organization is volunteer-ready with Volunteer Ally. Our system allows you to easily post your volunteer opportunities and have them easily found by qualified volunteers – all for free! </Item.Description>
+
+        <Item.Description style={textStyle3}> Here are some of the great features you’ll find with Volunteer Ally: </Item.Description>
+
+        <Grid style={gridStyle}>
+            <Grid.Column width={.2}>
+        <Icon name='check' size='big' style={iconStyle}/>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Item.Description style={iconTextStyle}> Access to hundreds of volunteers with a wide range of skills and availability </Item.Description>
+            </Grid.Column>
+        </Grid>
+
+        <Grid>
+            <Grid.Column width={.2}>
+                <Icon name='check' size='big' style={iconStyle}/>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Item.Description style={iconTextStyle}> Direct opportunity RSVPs to your inbox </Item.Description>
+            </Grid.Column>
+        </Grid>
+
+        <Grid>
+            <Grid.Column width={.2}>
+                <Icon name='check' size='big' style={iconStyle}/>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Item.Description style={iconTextStyle}> Database of volunteers and opportunities </Item.Description>
+            </Grid.Column>
+        </Grid>
+
+        <Grid>
+            <Grid.Column width={.2}>
+                <Icon name='check' size='big' style={iconStyle}/>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Item.Description style={iconTextStyle}> Integration-ready </Item.Description>
+            </Grid.Column>
+        </Grid>
+        <Grid centered columns={1} style={gridStyle2}>
+        <Button size='huge' color='blue'>Sign Up Today</Button>
         </Grid>
     </div>
 );
