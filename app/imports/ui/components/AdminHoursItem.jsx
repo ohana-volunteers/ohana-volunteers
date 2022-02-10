6136aeeb9d0e0f64f9cf2c124ca0bbf6cc60ad3e
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2022-02-09 22:43:18 -1000

    issue #40 AdminHoursItem component

---
+++ app/imports/ui/components/AdminHoursItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+/** Renders a single row in the List Hours table. See pages/ListHoursAdmin.jsx. */
+const AdminHoursItem = ({ Hours }) => (
+  <Table.Row>
+    <Table.Cell>{Hours.eventID}</Table.Cell>
+    <Table.Cell>{Hours.organization}</Table.Cell>
+    <Table.Cell>{Hours.date}</Table.Cell>
+    <Table.Cell>{Hours.hours}</Table.Cell>
+    <Table.Cell>{Hours.verifiedOn}</Table.Cell>
+  </Table.Row>
+);
+
+// Require a document to be passed to this component.
+AdminHoursItem.propTypes = {
+  Hours: PropTypes.shape({
+    eventID: PropTypes.string,
+    organization: PropTypes.string,
+    date: PropTypes.string,
+    hours: PropTypes.number,
+    verifiedOn: PropTypes.string,
+    isVerified: PropTypes.bool,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default AdminHoursItem;
