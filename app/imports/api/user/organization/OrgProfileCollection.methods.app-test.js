600e5923b584f813de133b765528b80da0ea633c
Stephanie Aelmore <49772454+believeinlain@users.noreply.github.com>
2022-03-03 13:22:13 -1000

    more progress towards collection testing

--- app/imports/api/user/organization/OrgProfileCollection.methods.app-test.js
+++ app/imports/api/user/organization/OrgProfileCollection.methods.app-test.js
@@ -4,6 +4,7 @@ import faker from 'faker';
 import { Organizations } from './OrgProfileCollection';
 import { defineTestUser, withLoggedInUser, withSubscriptions } from '../../test-utilities/test-utilities';
 import { defineMethod, updateMethod, removeItMethod } from '../base/BaseCollection.methods';
+import { getVolunteerCategoryNames } from '../../utilities/VolunteerCategories';
 
 /* eslint prefer-arrow-callback: "off",  no-unused-expressions: "off" */
 /* eslint-env mocha */
@@ -11,40 +12,42 @@ import { defineMethod, updateMethod, removeItMethod } from '../base/BaseCollecti
 if (Meteor.isClient) {
   describe('OrgCollection Meteor Methods', function testSuite() {
     it('Can define, update, and removeIt', async function test1() {
-      const { username, password } = await defineTestUser.callPromise();
-      await withLoggedInUser({ username, password });
+      // set up for testing
+      const { email, password } = await defineTestUser.callPromise();
+      await withLoggedInUser({ email, password });
       await withSubscriptions();
       const collectionName = Organizations.getCollectionName();
-      const definitionData = {};
-      definitionData.name = faker.lorem.words();
-      definitionData.quantity = faker.datatype.number({
-        min: 1,
-        max: 10,
-      });
-      definitionData.owner = username;
-      definitionData.condition = stuffConditions[faker.datatype.number({ min: 0, max: stuffConditions.length - 1 })];
-      // console.log(collectionName, definitionData);
+
+      // test define method
+      const definitionData = {
+        name: faker.lorem.words(),
+        categories: getVolunteerCategoryNames().sort(() => 0.5 - Math.random()).slice(0, 2),
+        owner: email,
+      };
+      console.log(collectionName, definitionData);
+
       const docID = await defineMethod.callPromise({ collectionName, definitionData });
-      expect(Stuffs.isDefined(docID)).to.be.true;
-      let doc = Stuffs.findDoc(docID);
+      expect(Organizations.isDefined(docID)).to.be.true;
+      let doc = Organizations.findDoc(docID);
       expect(doc.name).to.equal(definitionData.name);
-      expect(doc.quantity).to.equal(definitionData.quantity);
-      expect(doc.condition).to.equal(definitionData.condition);
-      const updateData = {};
-      updateData.id = docID;
-      updateData.name = faker.lorem.words();
-      updateData.quantity = faker.datatype.number({
-        min: 1,
-        max: 10,
-      });
-      updateData.condition = stuffConditions[faker.datatype.number({ min: 1, max: stuffConditions.length - 1 })];
+      expect(doc.categories).to.equal(definitionData.categories);
+      expect(doc.owner).to.equal(definitionData.owner);
+
+      // test update functionality
+      const updateData = {
+        name: faker.lorem.words(),
+        categories: getVolunteerCategoryNames().sort(() => 0.5 - Math.random()).slice(0, 2),
+        owner: email,
+      };
       await updateMethod.callPromise({ collectionName, updateData });
-      doc = Stuffs.findDoc(docID);
-      expect(doc.name).to.equal(updateData.name);
-      expect(doc.quantity).to.equal(updateData.quantity);
-      expect(doc.condition).to.equal(updateData.condition);
+      doc = Organizations.findDoc(docID);
+      expect(doc.name).to.equal(definitionData.name);
+      expect(doc.categories).to.equal(definitionData.categories);
+      expect(doc.owner).to.equal(definitionData.owner);
+
+      // test removal
       await removeItMethod.callPromise({ collectionName, instance: docID });
-      expect(Stuffs.isDefined(docID)).to.be.false;
+      expect(Organizations.isDefined(docID)).to.be.false;
     });
   });
 }
