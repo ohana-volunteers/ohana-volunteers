b248b84ad1c8828c8a386cf782d4afbe08ad3adc
Stephanie Aelmore <49772454+believeinlain@users.noreply.github.com>
2022-03-23 16:30:05 -1000

    updated OrganizationLibrary.jsx to actually use the organizations collection

--- app/imports/ui/pages/OrganizationLibrary.jsx
+++ app/imports/ui/pages/OrganizationLibrary.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import { Grid, Divider, Item, Icon, Button, Container, Image } from 'semantic-ui-react';
+import { Grid, Divider, Item, Icon, Container, Image, Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Organizations } from '../../api/user/organization/OrgProfileCollection';
 
 const container1Style = { backgroundColor: 'teal', paddingBottom: '325px', marginTop: '-25px', paddingLeft: '0px' };
-const container2Style = { backgroundColor: 'black', paddingBottom: '325px', marginTop: '40px', paddingLeft: '0px', marginBottom: '-23px' };
 const bodyStyle = { backgroundColor: 'rgba(0, 255, 255, .1)', marginBottom: '-73px' };
 const textStyle = { color: 'white', marginTop: '120px', fontSize: '55px', fontFamily: 'Papyrus' };
 const textStyle2 = { color: 'teal', textAlign: 'center', marginTop: '40px', fontSize: '40px', fontFamily: 'Copperplate' };
 const textStyle3 = { color: 'teal', textAlign: 'center', paddingTop: '50px', marginTop: '20px', fontSize: '20px', fontFamily: 'Copperplate' };
 const textStyle4 = { color: 'teal', textAlign: 'center', paddingTop: '50px', marginTop: '0px', fontSize: '20px', fontFamily: 'Copperplate' };
 const textStyle5 = { color: 'white', fontSize: '15px' };
-const textStyle6 = { color: 'black', fontSize: '55px', fontFamily: 'Papyrus' };
 const textStyle7 = { color: 'black', fontSize: '55px', fontFamily: 'Papyrus', marginBottom: '-50px' };
 const iconStyle = { paddingLeft: '115px' };
-const iconStyle2 = { paddingLeft: '300px' };
 const iconTextStyle = { paddingLeft: '80px', fontsize: '30px', fontFamily: 'Copperplate', color: 'teal' };
 const gridStyle = { marginTop: '100px' };
-const gridStyle2 = { paddingTop: '150px', paddingBottom: '120px' };
 const gridStyle3 = { marginTop: '50px' };
 const gridStyle4 = { marginTop: '50px', marginBottom: '50px' };
 const marginTop = { marginTop: '100px' };
 
 /** A simple static component to render some text for the About Us page. */
 
-const OrganizationLibrary = () => (
+const OrganizationLibrary = ({ orgs, ready }) => (
 
   <div style={bodyStyle}>
     <Divider style={container1Style}>
@@ -33,68 +32,19 @@ const OrganizationLibrary = () => (
       </Container>
     </Divider>
 
-    <Grid container centered columns={4} style={marginTop}>
-      <Grid.Column>
-        <Image src='/images/aloha-animal-sanctuary/org-logos-01-150x150.png' size='small'/>
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/habitat-for-humanity/org-logos-02-150x150.png' size='small'/>
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/hawaii-foodbank/org-logos-03-150x150.png' size='small' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='/images/meals-on-wheels/org-logos-04-150x150.png' size='small'/>
-      </Grid.Column>
+    {(ready) ? (
+      <Grid centered columns={4}>
+        {orgs.map((org) => (
+          <Image
+            key={org._id} as='a'
+            href={`#/organization-profile/${org._id}`}
+            src={org.logo_mini}
+          />))
+        }
+      </Grid>
+    ) : <Loader active>Fetching Organizations...</Loader>}
 
-      <Grid.Row centered columns={4}>
-        <Grid.Column>
-          <Image src='/images/org-logos-05-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-06-150x150.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-07-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-08-150x150.png'/>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row centered columns={4}>
-        <Grid.Column>
-          <Image src='/images/org-logos-09-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-10-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-11-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-12-150x150.png'/>
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row centered columns={4}>
-        <Grid.Column>
-          <Image src='/images/org-logos-13-150x150.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-14-150x150.png'/>
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-15-150x150.png' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='/images/org-logos-16-150x150.png' />
-        </Grid.Column>
-      </Grid.Row>
-
-    </Grid>
-
-    <Image centered src='/images/elissa-garcia-MV1l4f_f1os-unsplash-2048x1365.jpg' fluid size='massive' style={marginTop}/>
+    <Image centered src='/images/elissa-garcia-MV1l4f_f1os-unsplash-2048x1365.jpg' size='massive' style={marginTop}/>
 
     <Grid centered columns={16} style={gridStyle3}>
       <Grid.Column>
@@ -122,7 +72,7 @@ const OrganizationLibrary = () => (
     <Item.Description style={textStyle4}> Here are some of the great features you’ll find with Volunteer Ally: </Item.Description>
 
     <Grid style={gridStyle}>
-      <Grid.Column width={0.2}>
+      <Grid.Column>
         <Icon name='check' size='big' style={iconStyle}/>
       </Grid.Column>
       <Grid.Column width={10}>
@@ -131,7 +81,7 @@ const OrganizationLibrary = () => (
     </Grid>
 
     <Grid>
-      <Grid.Column width={0.2}>
+      <Grid.Column>
         <Icon name='check' size='big' style={iconStyle}/>
       </Grid.Column>
       <Grid.Column width={10}>
@@ -140,7 +90,7 @@ const OrganizationLibrary = () => (
     </Grid>
 
     <Grid>
-      <Grid.Column width={0.2}>
+      <Grid.Column>
         <Icon name='check' size='big' style={iconStyle}/>
       </Grid.Column>
       <Grid.Column width={10}>
@@ -149,7 +99,7 @@ const OrganizationLibrary = () => (
     </Grid>
 
     <Grid>
-      <Grid.Column width={0.2}>
+      <Grid.Column>
         <Icon name='check' size='big' style={iconStyle}/>
       </Grid.Column>
       <Grid.Column width={10}>
@@ -167,4 +117,22 @@ const OrganizationLibrary = () => (
   </div>
 );
 
-export default OrganizationLibrary;
+// Require an array of documents in the props.
+OrganizationLibrary.propTypes = {
+  orgs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to documents.
+  const subscription = Organizations.subscribe();
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the documents and sort them by name.
+  const orgs = Organizations.find({}, { sort: { name: 1 } }).fetch();
+  return {
+    orgs,
+    ready,
+  };
+})(OrganizationLibrary);
