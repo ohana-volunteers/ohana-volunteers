55effa590f849a724a21b40d8cf462d6750af824
Stephanie Aelmore <49772454+believeinlain@users.noreply.github.com>
2022-03-23 16:51:49 -1000

    updated OrgProfileCollection.methods.app-test.js to pass unit tests

--- app/imports/api/user/organization/OrgProfileCollection.methods.app-test.js
+++ app/imports/api/user/organization/OrgProfileCollection.methods.app-test.js
@@ -4,7 +4,6 @@ import faker from 'faker';
 import { Organizations } from './OrgProfileCollection';
 import { signUpNewOrganizationMethod } from './OrgProfileCollection.methods';
 import { getVolunteerCategoryNames } from '../../utilities/VolunteerCategories';
-import { defineTestAdminUser, withLoggedInUser, withSubscriptions } from '../../../test-utilities/test-utilities';
 
 /* eslint prefer-arrow-callback: "off",  no-unused-expressions: "off" */
 /* eslint-env mocha */
@@ -13,21 +12,17 @@ if (Meteor.isClient) {
   describe('OrgCollection Meteor Methods', function testSuite() {
     it('Can signUpNewOrganization', async function test1() {
       // set up for testing
-      const { email, password } = await defineTestAdminUser.callPromise();
-      await withLoggedInUser({ email, password });
-      await withSubscriptions();
-
-      // test define method
       const user = {
-        username: email,
-        password: password,
+        email: faker.internet.email(),
+        password: faker.internet.password(),
       };
       const profile = {
         name: faker.lorem.words(),
         categories: getVolunteerCategoryNames().sort(() => 0.5 - Math.random()).slice(0, 2),
-        owner: email,
+        owner: user.email,
       };
 
+      // test define method
       const docID = await signUpNewOrganizationMethod.callPromise({ user, profile });
       expect(Organizations.isDefined(docID)).to.be.true;
       const doc = Organizations.findDoc(docID);
