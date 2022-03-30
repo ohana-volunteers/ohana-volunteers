546e29a9a8312c6b5196568f43d872a840651161
yongxin <yongxin@hawaii.edu>
2022-03-29 23:37:42 -1000

    Have Google maps open in a new tab

--- app/imports/ui/pages/OpportunityDetails.jsx
+++ app/imports/ui/pages/OpportunityDetails.jsx
@@ -12,114 +12,122 @@ import { decode } from '../utilities/ImageDecode';
 /** A simple component to render some text for an Opportunity and its details. */
 const MAPBOX_TOKEN = 'pk.eyJ1IjoieW9uZ3hpbjAwNTYiLCJhIjoiY2t6cm1ueTkzNnY2dTJvbmszZmhtcHo1cSJ9.jpI6brR6TtGGMO9erkuV8g';
 
-const OpportunityDetails = ({ doc, ready }) => ((ready) ? (
-  <Container id={PAGE_IDS.OPPORTUNITY_DETAILS}>
-    <Card fluid>
-      {/* Temp image */}
-      <Image className="volunteer-bg-banner" src="/images/default_opp_header.jpeg"/>
-      <Card.Content>
-        <Grid container row={1}>
-          <Grid.Row columns={2}>
-            <Grid.Column width={10}>
-              <Card.Header as="h1">
-                <Image size="tiny" src={decode(doc.img)}/> {doc.event}
-                <br/>
-              </Card.Header>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <List size="massive">
-                <List.Item><b>Organization Host:</b> {doc.organization}</List.Item>
-                <List.Item><b>Date: </b>{doc.date.start.toISOString().slice(0, 10).concat('  ')}
-                  {doc.date.start.toISOString().slice(11, 16).concat('  ')} to <br/> {doc.date.end.toISOString().slice(0, 10).concat('  ')}
-                  {doc.date.end.toISOString().slice(11, 16)}</List.Item>
-              </List>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Card.Content>
-      <Card.Content>
-        <Grid columns={2}>
-          <Grid.Column>
-            <Grid.Row style={{ paddingBottom: '5px' }}>
-              <Button color='blue' size="small" href={`https://www.google.com/maps/place/${doc.address}`}>
-                <Icon name='map' />
-              Get Direction
-              </Button>
+const OpportunityDetails = ({ doc, ready }) => {
+  const openNewTab = () => {
+    const link = `https://www.google.com/maps/place/${doc.address}`;
+    // eslint-disable-next-line no-undef
+    window.open(link);
+  };
+  return (ready) ? (
+    <Container id={PAGE_IDS.OPPORTUNITY_DETAILS}>
+      <Card fluid>
+        {/* Temp image */}
+        <Image className="volunteer-bg-banner" src="/images/default_opp_header.jpeg"/>
+        <Card.Content>
+          <Grid container row={1}>
+            <Grid.Row columns={2}>
+              <Grid.Column width={10}>
+                <Card.Header as="h1">
+                  <Image size="tiny" src={decode(doc.img)}/> {doc.event}
+                  <br/>
+                </Card.Header>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <List size="massive">
+                  <List.Item><b>Organization Host:</b> {doc.organization}</List.Item>
+                  <List.Item><b>Date: </b>{doc.date.start.toISOString().slice(0, 10).concat('  ')}
+                    {doc.date.start.toISOString().slice(11, 16).concat('  ')} to <br/> {doc.date.end.toISOString().slice(0, 10).concat('  ')}
+                    {doc.date.end.toISOString().slice(11, 16)}</List.Item>
+                </List>
+              </Grid.Column>
             </Grid.Row>
-            <Grid.Row >
-              {(doc.coordinates) ? (
-                <Map
+          </Grid>
+        </Card.Content>
+        <Card.Content>
+          <Grid columns={2}>
+            <Grid.Column>
+              <Grid.Row style={{ paddingBottom: '5px' }}>
+                <Button color='blue' size="small" onClick={openNewTab}>
+                  <Icon name='map'/>
+                    Get Google Maps Directions
+                </Button>
+              </Grid.Row>
+              <Grid.Row>
+                {(doc.coordinates) ? (
+                  <Map
+                    initialViewState={{
+                      latitude: doc.coordinates.lat,
+                      longitude: doc.coordinates.log,
+                      zoom: 14,
+                    }}
+                    style={{ width: 500, height: 400 }}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    mapboxAccessToken={MAPBOX_TOKEN}
+                  >
+                    <Marker longitude={doc.coordinates.log} latitude={doc.coordinates.lat} color="red"/>
+                  </Map>) : (<Map
                   initialViewState={{
-                    latitude: doc.coordinates.lat,
-                    longitude: doc.coordinates.log,
+                    latitude: 21.315603,
+                    longitude: -157.858093,
                     zoom: 14,
                   }}
                   style={{ width: 500, height: 400 }}
                   mapStyle="mapbox://styles/mapbox/streets-v9"
                   mapboxAccessToken={MAPBOX_TOKEN}
-                >
-                  <Marker longitude={doc.coordinates.log} latitude={doc.coordinates.lat} color="red"/>
-                </Map>) : (<Map
-                initialViewState={{
-                  latitude: 21.315603,
-                  longitude: -157.858093,
-                  zoom: 14,
-                }}
-                style={{ width: 500, height: 400 }}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-                mapboxAccessToken={MAPBOX_TOKEN}
-              />)}
-            </Grid.Row>
-          </Grid.Column>
-          <Grid.Column>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header><Icon name="map"/> Address </Card.Header>
-              </Card.Content>
-              <Card.Content>
-                <List size="big">
-                  <List.Item>{doc.address}</List.Item>
-                </List>
-              </Card.Content>
-            </Card>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header><Icon name="columns"/> Categories </Card.Header>
-              </Card.Content>
-              <Card.Content>
-                <List as="ul" size="big">
-                  {doc.categories.map((item, index) => <List.Item key={index} as="li">{volunteerCategories[item].name}</List.Item>)}
-                </List>
-              </Card.Content>
-            </Card>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header><Icon name="tree"/> Environment</Card.Header>
-              </Card.Content>
-              <Card.Content>
-                <List as="ul" size="big">
-                  <List.Item as="li">{doc.environment}</List.Item>
-                </List>
-              </Card.Content>
-            </Card>
-            <Card fluid>
-              <Card.Content>
-                <Card.Header><Icon name="user"/> Age Group</Card.Header>
-              </Card.Content>
-              <Card.Content>
-                <List as="ul" size="big">
-                  <List.Item as="li">{doc.age}</List.Item>
-                </List>
-              </Card.Content>
-            </Card>
-            {/* Button currently non-functional */}
-            <Button primary fluid size="large">Register With Organization Host</Button>
-          </Grid.Column>
-        </Grid>
-      </Card.Content>
-    </Card>
-  </Container>
-) : <Loader active>Getting data</Loader>);
+                />)}
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column>
+              <Card fluid>
+                <Card.Content>
+                  <Card.Header><Icon name="map"/> Address </Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <List size="big">
+                    <List.Item>{doc.address}</List.Item>
+                  </List>
+                </Card.Content>
+              </Card>
+              <Card fluid>
+                <Card.Content>
+                  <Card.Header><Icon name="columns"/> Categories </Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <List as="ul" size="big">
+                    {doc.categories.map((item, index) => <List.Item key={index}
+                      as="li">{volunteerCategories[item].name}</List.Item>)}
+                  </List>
+                </Card.Content>
+              </Card>
+              <Card fluid>
+                <Card.Content>
+                  <Card.Header><Icon name="tree"/> Environment</Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <List as="ul" size="big">
+                    <List.Item as="li">{doc.environment}</List.Item>
+                  </List>
+                </Card.Content>
+              </Card>
+              <Card fluid>
+                <Card.Content>
+                  <Card.Header><Icon name="user"/> Age Group</Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <List as="ul" size="big">
+                    <List.Item as="li">{doc.age}</List.Item>
+                  </List>
+                </Card.Content>
+              </Card>
+              {/* Button currently non-functional */}
+              <Button primary fluid size="large">Register With Organization Host</Button>
+            </Grid.Column>
+          </Grid>
+        </Card.Content>
+      </Card>
+    </Container>
+  ) : <Loader active>Getting opp</Loader>;
+};
 
 OpportunityDetails.propTypes = {
   doc: PropTypes.object,
