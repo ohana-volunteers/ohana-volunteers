068d9bb78051c52455d45b027c38f17105b86a5f
Joshua Paino <62793439+joshipaino@users.noreply.github.com>
2022-04-11 14:34:44 -1000

    search for the BrowseOpportunities admin
    need to fix/expand sorting

--- app/imports/ui/pages/BrowseOpportunitiesAdmin.jsx
+++ app/imports/ui/pages/BrowseOpportunitiesAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { NavLink } from 'react-router-dom';
-import { Grid, Header, Divider, Card, Loader, Segment, Icon, Button, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Divider, Card, Loader, Segment, Icon, Button, Dropdown, Input } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { PAGE_IDS } from '../utilities/PageIDs';
@@ -13,6 +13,7 @@ const headerStyle = { color: 'white', backgroundColor: 'teal', padding: 10 };
 // const buttonStyle = { color: 'black', backgroundColor: 'white', borderRadius: 3, padding: 5, borderColor: 'blue' };
 
 const BrowseOpportunitiesAdmin = ({ expiredOpps, activeOpps, notVerifiedOpps, ready, currentUser }) => {
+  const [oppSearch, setOppSearch] = useState('');
   const [order, setOrder] = useState('');
 
   const handleSelect = (e, { value }) => {
@@ -22,31 +23,79 @@ const BrowseOpportunitiesAdmin = ({ expiredOpps, activeOpps, notVerifiedOpps, re
   const sortedExpiredOpps = Opportunities.find({ $and: [{ isVerified: true }, { 'date.end': { $lt: toDate } }] }, sort);
   const sortedActiveOpps = Opportunities.find({ $and: [{ isVerified: true }, { 'date.end': { $gte: toDate } }] }, sort);
   const sortedNotVerifiedOpps = Opportunities.find({ isVerified: false }, sort);
+
+  const searchExpiredOpps = expiredOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  });
+  const searchActiveOpps = activeOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  });
+  const searchNotVerifiedOpps = notVerifiedOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  });
+
+  /*  const searchSortedExpiredOpps = sortedExpiredOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  });
+  const searchSortedActiveOpps = sortedActiveOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  });
+  const searchSortedNotVerfiedOpps = sortedNotVerifiedOpps.filter((val) => {
+    if (oppSearch === '') {
+      return val;
+    }
+    return val.organization.toLowerCase().includes(oppSearch);
+  }); */
   return (ready) ? (
-    <Grid id={PAGE_IDS.BROWSE_OPPORTUNITIES_ADMIN} textAlign='center' container>
+    <Grid id={PAGE_IDS.BROWSE_OPPORTUNITIES_ADMIN} container centered>
       <Grid.Row centered>
         <Header as="h1" >Manage Opportunities</Header>
+      </Grid.Row>
+      <Divider/>
+      <Grid.Row columns={2}>
+        <Grid.Column width={14}>
+          <Input
+            fluid
+            icon='search'
+            placeholder="Search Opportunities (Active/Expired/Pending)"
+            onChange={(e) => setOppSearch(e.target.value)}/>
+        </Grid.Column>
+        <Grid.Column width={2}>
+          <Dropdown text='Sort By'
+            icon='angle down'
+            floating
+            className='icon'>
+            <Dropdown.Menu>
+              <Dropdown.Item icon='calendar outline' text='A-Z' value='A-Z' onClick={handleSelect}/>
+              <Dropdown.Item icon='sort alphabet down' text='Latest' value='Latest' onClick={handleSelect}/>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Grid.Column>
       </Grid.Row>
       <Divider/>
       <Grid.Row centered>
         <Header as="h2" style={ headerStyle }><Icon name='calendar check outline'/>Active Opportunities ({activeOpps.length})</Header>
       </Grid.Row>
-      <Grid.Row>
-        <Dropdown text='Sort By'
-          icon='angle down'
-          floating
-          className='icon'>
-          <Dropdown.Menu>
-            <Dropdown.Item icon='calendar outline' text='A-Z' value='A-Z' onClick={handleSelect}/>
-            <Dropdown.Item icon='sort alphabet down' text='Latest' value='Latest' onClick={handleSelect}/>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Grid.Row>
       <Segment vertical padded='very' className='line'>
-        <Grid.Row>
+        <Grid.Row centered>
           <Card.Group centered>
             {(order === '') ?
-              activeOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
+              searchActiveOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
               sortedActiveOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>)
             }
           </Card.Group>
@@ -56,10 +105,10 @@ const BrowseOpportunitiesAdmin = ({ expiredOpps, activeOpps, notVerifiedOpps, re
         <Header as="h2" style={ headerStyle }> <Icon name='empire'/>Expired Opportunities ({expiredOpps.length})</Header>
       </Grid.Row>
       <Segment vertical padded='very' className='line'>
-        <Grid.Row>
+        <Grid.Row centered>
           <Card.Group centered className='expired-card'>
             {(order === '') ?
-              expiredOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
+              searchExpiredOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
               sortedExpiredOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>)
             }
           </Card.Group>
@@ -69,16 +118,16 @@ const BrowseOpportunitiesAdmin = ({ expiredOpps, activeOpps, notVerifiedOpps, re
         <Header as="h2" style={ headerStyle }> <Icon name='zoom'/>Pending Opportunities ({notVerifiedOpps.length})</Header>
       </Grid.Row>
       <Segment vertical padded='very' className='line'>
-        <Grid.Row>
+        <Grid.Row centered>
           <Card.Group centered>
             {(order === '') ?
-              notVerifiedOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
+              searchNotVerifiedOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>) :
               sortedNotVerifiedOpps.map((opp) => <OpportunityItem key={opp._id} opp={opp} user={currentUser}/>)
             }
           </Card.Group>
         </Grid.Row>
       </Segment>
-      <Grid.Row>
+      <Grid.Row centered>
         <Button as={NavLink} exact to='/browse-opportunities'>Browse Opportunities as Volunteers</Button>
       </Grid.Row>
     </Grid>
