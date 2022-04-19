01f66b9283ac978b8e5bfa5b70b58cd570986dd9
Andrew Emmons <60409142+andrewemmons95@users.noreply.github.com>
2022-04-18 20:31:34 -1000

    changed layout on landing page as well as updated the organization library

--- app/imports/ui/pages/OrganizationLibrary.jsx
+++ app/imports/ui/pages/OrganizationLibrary.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Grid, Divider, Item, Icon, Button, Container, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom'
 
-const container1Style = { backgroundColor: 'teal', paddingBottom: '325px', marginTop: '-25px', paddingLeft: '0px' };
-const bodyStyle = {backgroundColor: 'rgba(0, 255, 255, .1)', marginBottom: '-73px' }
-const textStyle = { color: 'white', marginTop: '120px', fontSize: '55px', fontFamily: 'Papyrus' };
-const textStyle2 = { color: 'teal', textAlign: 'center', marginTop: '40px', fontSize: '40px', fontFamily: 'Copperplate' };
-const textStyle3 = { color: 'teal', textAlign: 'center', paddingTop: '50px', marginTop: '20px', fontSize: '20px', fontFamily: 'Copperplate' };
-const textStyle4 = { color: 'teal', textAlign: 'center', paddingTop: '50px', marginTop: '0px', fontSize: '20px', fontFamily: 'Copperplate' };
-const textStyle5 = { color: 'white', fontSize: '15px' };
-const textStyle7 = { color: 'black', fontSize: '55px', fontFamily: 'Papyrus', marginBottom: '-50px' };
+const container1Style = { backgroundColor: 'black', paddingBottom: '345px', marginTop: '-25px', paddingLeft: '0px', marginBottom: '25px' };
+const bodyStyle = {backgroundColor: 'rgba(0, 255, 255, .1)', marginBottom: '-50px' };
+const textStyle = { color: 'white', marginTop: '120px', fontSize: '75px', fontFamily: 'Copperplate' };
+const textStyle2 = { color: 'black', textAlign: 'center', marginTop: '40px', fontSize: '40px', fontFamily: 'Copperplate' };
+const textStyle3 = { color: 'black', textAlign: 'center', paddingTop: '50px', marginTop: '20px', fontSize: '20px', fontFamily: 'Copperplate' };
+const textStyle4 = { color: 'black', textAlign: 'center', paddingTop: '50px', marginTop: '0px', fontSize: '20px', fontFamily: 'Copperplate' };
+const textStyle5 = { color: 'white', fontSize: '25px', fontFamily: 'Copperplate' };
+const textStyle7 = { color: 'black', fontSize: '55px', fontFamily: 'Copperplate', marginBottom: '-50px' };
 const iconStyle = { paddingLeft: '115px' };
-const iconTextStyle = { paddingLeft: '80px', fontsize: '30px', fontFamily: 'Copperplate', color: 'teal' };
+const iconTextStyle = { paddingLeft: '80px', fontsize: '30px', fontFamily: 'Copperplate', color: 'black' };
 const gridStyle = { marginTop: '100px' };
 const gridStyle3 = { marginTop: '50px' };
 const gridStyle4 = { marginTop: '50px', marginBottom: '50px' };
-const marginTop = { marginTop: '100px' };
+const marginTop = { marginTop: '50px' };
 const style = {marginTop: '100px' };
 
 /** A simple static component to render some text for the About Us page. */
@@ -95,24 +95,26 @@ const OrganizationLibrary = () => (
 
         <Grid centered columns={16} style={gridStyle3}>
             <Grid.Column>
-                <Icon name='ellipsis horizontal' size='huge'/>
+                <Icon name='circle' size='big'/>
             </Grid.Column>
         </Grid>
 
         <Item.Header as="h1" style={textStyle2}> Join over 20 organizations already finding the help they need with Volunteer Ally. </Item.Header>
 
+
         <Grid centered columns={16} style={gridStyle3}>
             <Grid.Column>
-                <Icon name='ellipsis horizontal' size='huge'/>
+                <Icon name='circle' size='big'/>
             </Grid.Column>
         </Grid>
+
 
         <Item.Description style={textStyle3}> There are thousands of active volunteers waiting for opportunities to work with qualified organizations.
             Make sure your organization is volunteer-ready with Volunteer Ally. Our system allows you to easily post your volunteer opportunities and have them easily found by qualified volunteers – all for free! </Item.Description>
 
         <Grid centered columns={16} style={gridStyle3}>
             <Grid.Column>
-                <Icon name='ellipsis horizontal' size='huge'/>
+                <Icon name='circle' size='big'/>
             </Grid.Column>
         </Grid>
 
@@ -137,7 +139,7 @@ const OrganizationLibrary = () => (
         </Grid>
 
         <Grid>
-            <Grid.Column width={0.2}>
+             <Grid.Column width={0.2}>
                 <Icon name='check' size='big' style={iconStyle}/>
             </Grid.Column>
             <Grid.Column width={10}>
@@ -158,7 +160,7 @@ const OrganizationLibrary = () => (
         </Container>
         <Grid centered columns={16} style={gridStyle4}>
             <Grid.Column>
-                <Link to={"/signup"}> <Icon name='handshake outline' size='huge'/></Link>
+                <Link to={"/signup"}> <Icon name='signup' size='huge'/></Link>
             </Grid.Column>
         </Grid>
 
