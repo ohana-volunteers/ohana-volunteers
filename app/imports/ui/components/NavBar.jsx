2116162d498b1f1dcbddeb32c4ca36e552946d6c
Andrew Emmons <60409142+andrewemmons95@users.noreply.github.com>
2022-04-26 19:17:47 -1000

    updated fonts, and corrector errors

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,22 +13,23 @@ import { ROLE } from '../../api/role/Role';
 import { useState } from 'react';
 
 /** The NavBar appears at the top of every page.  Access to certain items is dependent on the user role. Rendered by the App Layout component. */
-const NavBar = ({ role, currentUser, orgs, opps,}) => {
+const fontStyle = {fontFamily: 'Copperplate'};
+const NavBar = ({ role, currentUser, opps,}) => {
   const menuStyle = { marginBottom: '25px', backgroundColor: 'rgba(0, 255, 255, .1)' };
   const input = { width: '20rem' };
-  var nameList = opps;
-  const orgsArray = [];
+  var nameList[] = opps;
+  const oppsArray = [];
   nameList.forEach((item) => {
-        orgsArray.push(item);
+        oppsArray.push(item);
       }
   );
-  const orgsLength = orgs.length;
+  const oppsLength = opps.length;
   const [inputText, setInputText] = useState("");
 
   var myList = new Array();
-  for (let i = 0; i < orgsLength; i++) {
-    let temp = orgsArray[i];
-    if (temp.event.toLowerCase().includes(inputText.toLowerCase()) && inputText != '')
+  for (let i = 0; i < oppsLength; i++) {
+    let temp = oppsArray[i];
+    if (temp.event.toLowerCase().includes(inputText.toLowerCase()) && inputText !== '')
     {
       myList.push(temp);
       console.log(myList);
@@ -40,7 +41,8 @@ const NavBar = ({ role, currentUser, orgs, opps,}) => {
       <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} exact to="/">
         <Image src="/images/va-logo/VA-logo-circle-v5.svg" size="tiny"/>
       </Menu.Item>
-      <Menu.Item id={COMPONENT_IDS.NAVBAR_SEARCH}><Input transparent style={input} icon="search" iconPosition="left" size="large" placeholder="Search for an opportunity..." name="search" onChange={(event) => { setInputText(event.target.value); }}></Input></Menu.Item>
+      <Menu.Item id={COMPONENT_IDS.NAVBAR_SEARCH}><Input transparent style={input} icon="search" iconPosition="left" size="large" placeholder="Search for an opportunity..." name="search" onChange={(event) =>
+      { setInputText(event.target.value); }}></Input></Menu.Item>
 
       <Card>
         {myList.map(post => (
@@ -53,52 +55,52 @@ const NavBar = ({ role, currentUser, orgs, opps,}) => {
         ))}
       </Card>
 
-      <Menu.Item id={COMPONENT_IDS.NAVBAR_HOME} position="right" as={NavLink} exact to="/" key='home'>Home</Menu.Item>
+      <Menu.Item id={COMPONENT_IDS.NAVBAR_HOME} position="right" as={NavLink} exact to="/" key='home' style={fontStyle}>Home</Menu.Item>
       {(role !== ROLE.ORGANIZATION) ?
-        <Menu.Item id={COMPONENT_IDS.NAVBAR_ORGANIZATION_LIBRARY} as={NavLink} exact to="/organization-library" key='library'>Organizations</Menu.Item> : ''}
+        <Menu.Item id={COMPONENT_IDS.NAVBAR_ORGANIZATION_LIBRARY} as={NavLink} exact to="/organization-library" key='library' style={fontStyle}>Organizations</Menu.Item> : ''}
       {(role === ROLE.ADMIN) ?
-        <Menu.Item id={COMPONENT_IDS.NAVBAR_ADMIN_HOME} as={NavLink} exact to="/admin-home" key='admin-home'>Admin Dashboard</Menu.Item> : ''}
+        <Menu.Item id={COMPONENT_IDS.NAVBAR_ADMIN_HOME} as={NavLink} exact to="/admin-home" key='admin-home' style={fontStyle}>Admin Dashboard</Menu.Item> : ''}
       {(!role || role === ROLE.VOLUNTEER) ?
         <React.Fragment>
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_COMMUNITY_EVENT} as={NavLink} activeClassName="active" exact to="/event" key='event' >Calendar</Menu.Item>
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_BROWSE_OPPORTUNITIES} as={NavLink} exact to="/browse-opportunities" key='browse'>Opportunities</Menu.Item>
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_ABOUT_US} as={NavLink} exact to="/about-us" key='aboutUs'>About Us</Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_COMMUNITY_EVENT} as={NavLink} activeClassName="active" exact to="/event" key='event' style={fontStyle}>Calendar</Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_BROWSE_OPPORTUNITIES} as={NavLink} exact to="/browse-opportunities" key='browse' style={fontStyle}>Opportunities</Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_ABOUT_US} as={NavLink} exact to="/about-us" key='aboutUs' style={fontStyle}>About Us</Menu.Item>
         </React.Fragment> : ''}
       {/* If we have a defined role, show the signed in user and dropdown */}
       {(currentUser) ?
         <Menu.Item key="user">
           <Image src="/images/va-logo/VA-logo-circle-v5.svg" avatar/>
-          <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right">
+          <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} style={fontStyle} text={currentUser} pointing="top right">
             <Dropdown.Menu>
               {(role === ROLE.VOLUNTEER) ?
                 // Volunteer drop-down options
                 <React.Fragment>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADD_HOURS} text="Verify More Hours" as={NavLink} exact to="/addhours"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LIST_HOURS} text="Tracked Hours" as={NavLink} exact to="/listhours"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_MY_PROFILE} text="My Profile" as={NavLink} exact to="/my-profile"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" as={NavLink} exact to="/notfound"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADD_HOURS} text="Verify More Hours" style={fontStyle} as={NavLink} exact to="/addhours"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LIST_HOURS} text="Tracked Hours" style={fontStyle} as={NavLink} exact to="/listhours"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_MY_PROFILE} text="My Profile" style={fontStyle} as={NavLink} exact to="/my-profile"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" style={fontStyle} as={NavLink} exact to="/notfound"/>
                 </React.Fragment> : ''}
               {(role === ROLE.ADMIN) ?
                 // Admin drop-down options
                 <React.Fragment>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADMIN_DROPDOWN_SIGN_UP_ORGANIZATION} text="Sign up new organization" as={NavLink} exact to="/org-signup"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ADD_OPPORTUNITY} text="Add Opportunity" as={NavLink} exact to="/add-opportunity"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" as={NavLink} exact to="/notfound"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADMIN_DROPDOWN_SIGN_UP_ORGANIZATION} text="Sign up new organization" style={fontStyle} as={NavLink} exact to="/org-signup"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ADD_OPPORTUNITY} text="Add Opportunity" style={fontStyle} as={NavLink} exact to="/add-opportunity"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" style={fontStyle} as={NavLink} exact to="/notfound"/>
                 </React.Fragment> : ''}
               {(role === ROLE.ORGANIZATION) ?
                 // Org drop-down options
                 <React.Fragment>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ADD_OPPORTUNITY} text="Add Opportunity" as={NavLink} exact to="/add-opportunity"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_MY_ORGANIZATION_PROFILE} text="My Organization Profile" as={NavLink} exact to="/my-organization-profile"/>
-                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" as={NavLink} exact to="/notfound"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ADD_OPPORTUNITY} text="Add Opportunity" style={fontStyle} as={NavLink} exact to="/add-opportunity"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_MY_ORGANIZATION_PROFILE} text="My Organization Profile" style={fontStyle} as={NavLink} exact to="/my-organization-profile"/>
+                  <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DROPDOWN_ACCOUNT_SETTINGS} text="Account Settings" style={fontStyle} as={NavLink} exact to="/notfound"/>
                 </React.Fragment> : ''}
-              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Logout" as={NavLink} exact to="/signout"/>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Logout" style={fontStyle} as={NavLink} exact to="/signout"/>
             </Dropdown.Menu>
           </Dropdown>
         </Menu.Item> :
         <React.Fragment>
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_LOGIN_SIGN_UP} as={NavLink} exact to="/signup" key='signup'>Sign Up</Menu.Item>
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_LOGIN_SIGN_IN} as={NavLink} exact to="/signin" key='signin'><Icon name='user'/>Sign In</Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_LOGIN_SIGN_UP} as={NavLink} exact to="/signup" key='signup' style={fontStyle}>Sign Up</Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_LOGIN_SIGN_IN} as={NavLink} exact to="/signin" key='signin' style={fontStyle}><Icon name='user'/>Sign In</Menu.Item>
         </React.Fragment>
       }
     </Menu>
@@ -117,13 +119,11 @@ const NavBarContainer = withTracker(() => {
   let currentUser = '';
   // Get the current user
   if (Meteor.user()) currentUser = Meteor.user().username;
-  const subscriptionOpps = Opportunities.subscribeOpportunity();
   const subscriptionAdmin = AdminProfiles.subscribe();
   const subscriptionVolunteer = VolunteerProfiles.subscribe();
   const subscriptionOrg = Organizations.subscribe();
   const subscriptionOpp = Opportunities.subscribe();
   const ready = subscriptionAdmin.ready() && subscriptionVolunteer.ready() && subscriptionOrg.ready() && subscriptionOpp.ready();
-  const orgs = Organizations.find({}, { sort: { name: 1 } }).fetch();
   const opps = Opportunities.find({}, { sort: { organization: 1 } }).fetch();
 
   // Get the document
@@ -136,7 +136,6 @@ const NavBarContainer = withTracker(() => {
   return {
     role,
     currentUser,
-    orgs,
     opps,
   };
 })(NavBar);
