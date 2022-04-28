031623f899be4ba74070c5ed0fbbe995444a9f22
chowc89 <chowc89@hawaii.edu>
2022-04-28 01:08:17 -1000

    setting up

--- app/imports/ui/pages/CommunityEvent.jsx
+++ app/imports/ui/pages/CommunityEvent.jsx
@@ -8,24 +8,55 @@ const localizer = momentLocalizer(moment);
 
 const bodyStyle = { backgroundColor: 'rgba(0, 255, 255, .1)', marginBottom: '-22px', marginTop: '-25px' };
 
-/** A simple static component to render some text for the landing page. */
+const dfdate = '2015-03-01';
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const show = () => setOpen(true);
+  const close = () => setOpen(false);
+  const [eventToModal, setEventToModal] = useState({});
+};
+
+const handleModal = event => {
+  show();
+  setEventToModal(event);
+  console.log(eventToModal);
+};
+
+const eventStyleGetter = (event, start, end, isSelected) => {
+  const backgroundColor = event.bgColor;
+  const style = {
+    backgroundColor,
+  };
+  return {
+    style,
+  };
+};
+
+// /** A simple static component to render some text for the landing page. */
 class CommunityEvent extends React.Component {
 
   state = {
     events: [
       {
-        start: moment().toDate(),
-        end: moment()
-          .add(2, 'days')
-          .toDate(),
+        start: '2022-04-16 09:00:00',
+        end: '2022-04-16 12:00:00',
         title: 'Hawaii Foodbank',
       },
       {
-        start: moment().toDate(),
-        end: moment()
-          .add(1, 'hour')
-          .toDate(),
+        start: '2022-04-20T10:30:00',
+        end: '2022-04-20T12:30:00',
         title: 'a-Organization#1',
+      },
+      {
+        start: '2022-04-24T10:30:00',
+        end: '2022-04-24T12:30:00',
+        title: 'a-Organization#2',
+      },
+      {
+        start: '2022-04-28T10:30:00',
+        end: '2022-04-28T12:30:00',
+        title: 'a-Organization#3',
       },
     ],
   };
